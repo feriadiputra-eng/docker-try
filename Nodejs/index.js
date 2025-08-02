2d6d835a404622bf21b503d43244ea0cde6015fc
feriadi <feriadiputra05@gmail.com>
2025-08-02 12:06:35 +0700

    try

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 5000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Node.js + Docker! tolonggggg");
+  res.send("Hello from Node.js + Docker! rawr");
 });
 
 app.get("/rahasia", (req, res) => {
